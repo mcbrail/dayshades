879eee93c804251895eea81f0ba2a0fa5272d00d
mcbrail <mcbrail@yahoo.com>
2014-05-21 22:31:06 +0200

    front end config modified

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.2.16'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'rails_12factor'
 
 group :development do
 	gem 'sqlite3'
--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,7 @@ module Dayshades
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
@@ -58,5 +59,7 @@ module Dayshades
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
